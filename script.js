2f110356baf73bae517da1590a64b9df2398624c
Matthew Pollicito <mpollicito@yahoo.com>
2020-09-21 19:38:02 -0400

    Search API function

--- script.js
+++ script.js
@@ -1,44 +1,82 @@
 $(document).ready(function () {
-    var displayGames = $("api-result")
+    var createdisplay = function (data) {
+        // Create a new table row element
+        var tRow = $("<tr>");
 
-    // var settings = {
-    //     "async": true,
-    //     "crossDomain": true,
-    //     "url": "https://api.rawg.io/api/games?dates=2019-10-10,2020-10-10&ordering=-added",
-    //     "method": "GET",
-    //     "headers": {
-    //         "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
-    //         "x-rapidapi-key": "0a549e6324mshe13a8958db253e6p1afae2jsnf6a1f9cb6e11"
-    //     }
-    // }
+        // Methods run on jQuery selectors return the selector they we run on
+        // This is why we can create and save a reference to a td in the same statement we update its text
+        var nameTd = $("<td>").text(data.name);
+        var playtimeTd = $("<td>").text(data.playtime);
+        var ratingTd = $("<td>").text(data.rating);
 
-    // $.ajax(settings).done(function (response) {
-    //     console.log(response);
-    // });
-
-    function searchGames() {
-        var queryUrl = "https://api.rawg.io/api/games?dates=2019-10-10,2020-10-10&ordering=-added"
-        $.ajax({
-            type: "GET",
-            url: queryUrl
-        })
-            .then(function (response) {
-                console.log(response);
-                var games = "Game List " + displayGames;
-                var results = results.Array;
-                displayGames.append(games + results);
-            })
+        // Append the newly created table data to the table row
+        tRow.append(nameTd, playtimeTd, ratingTd);
+        // Append the table row to the table body
+        $("tbody").append(tRow);
     };
 
-    searchGames();
-})
+    var i = 0;
 
-// RAWG APIs:
-// https://rawg-video-games-database.p.rapidapi.com/platforms Use to search by platform
-// https://rawg-video-games-database.p.rapidapi.com/games  Use to search by game name
+    var anything = [""];
+    var searchGames = function (anything) {
+
+        // constructing a queryURL variable we will use instead of the literal string inside of the ajax method
+
+        var queryURL = "https://api.rawg.io/api/games?search=" + anything;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            var searchResults = response.results[i];
+
+            console.log(response.results[i]);
+            createdisplay(response.results[i]);
+
+            //OR
+
+            var gameInfo = $("<p>").text(response.results[i].name);
+            $(".container").append(gameInfo);
+
+            console.log(response.results[i].id);
+        });
+    };
+    // ---------------------------------------------------------
+    // searchGames("portal");
+    // searchGames("Halo");
+});
+
+https://api.rawg.io/docs/#tag/games 
 
 
+// var tag = document.createElement('script');
 
-// Need:
-// 2 Serverside APIs 
-// 3rd Party framework
+//     tag.src = "https://youtu.be/tjji8NEW9lo";
+//     var firstScriptTag = document.getElementsByTagName('script')[0];
+//     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+//     var player;
+//     function onYouTubeIframeAPIReady() {
+//         player = new YT.Player('player', {
+//             height: '390',
+//             width: '640',
+//             videoId: 'M7lc1UVf-VE',
+//             events: {
+//                 'onReady': onPlayerReady,
+//                 'onStateChange': onPlayerStateChange
+//             }
+//         });
+//     }
+//     function onPlayerReady(event) {
+//         event.target.playVideo();
+//     }
+//     var done = false;
+//     function onPlayerStateChange(event) {
+//         if (event.data == YT.PlayerState.PLAYING && !done) {
+//             setTimeout(stopVideo, 6000);
+//             done = true;
+//         }
+//     }
+//     function stopVideo() {
+//         player.stopVideo();
+//     }      YOUTUBE PLAYER <-----------------
